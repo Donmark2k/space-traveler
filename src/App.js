f5d5654bb353c68b6a111c681eb40c7634fb94a0
Donmark2k <chukwuemekavp@gmail.com>
2023-03-10 12:03:29 +0100

    Add the logo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Navbar from './components/navbar';
 import { Route, Routes} from 'react-router-dom';
+import Rocket from './components/rocket';
+import Mission from './components/mission';
+import Profile from './components/profile';
 
 function App() {
   return (
     <div className="App" >
     <Navbar/>
     <Routes>
-      <Route path="/"element={<Rocket/>} />;
+      <Route path="/"element={<Rocket/> } />;
       <Route path ="/missions" element = {<Mission/>} />;
       <Route path = "/profile" element = {<Profile/>} />;
     </Routes>
 
-    
     </div>
   );
 }
