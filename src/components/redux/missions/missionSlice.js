d2ace297eb391fdde521642f1e422e64db9e4396
Donmark2k <chukwuemekavp@gmail.com>
2023-03-10 02:18:11 +0100

    Create reducer for missions

---
+++ src/components/redux/missions/missionSlice.js
@@ -0,0 +1,18 @@
+import { createSlice , createAsyncThunk } from '@reduxjs/toolkit';
+
+
+const initialState = {
+    mission: [],
+}
+
+
+const missionSlice = createSlice({
+    name: 'missions',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+
+    }
+})
+
+export default missionSlice.reducer;
