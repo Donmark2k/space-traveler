c2b7adcfb54a07fad8f6867278efe8d3aa0d1def
Donmark2k <chukwuemekavp@gmail.com>
2023-03-11 06:08:16 +0100

    import button and badge from react-bootstrap

--- src/components/mission.js
+++ src/components/mission.js
@@ -1,9 +1,23 @@
 import React, { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import {useDispatch, useSelector} from 'react-redux';
+import { Badge } from 'react-bootstrap/Badge';
+import { fetchMissions } from './../redux/missions/missionSlice';
 
-const Mission = () => (
-  <h1>Mission</h1>
-);
+const Mission = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state)=>state.missions.status);
+  const missions = useSelector((state)=> state.missions.missionList);
+  useEffect(() => {
+    if (status==="idle") {
+      dispatch(fetchMissions());
+    }
+  },[status, dispatch])
+
+  return (
+    
+  )
+}
 
 export default Mission;
