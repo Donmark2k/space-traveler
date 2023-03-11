2893b89b048ea6decf4bd1e31a9e43bef9c54f0d
Donmark2k <chukwuemekavp@gmail.com>
2023-03-11 10:44:28 +0100

    Add test function for navbar

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -4,13 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 import NavBar from './components/NavBar';
-import Missions from './components/Missions';
 import store from './redux/store';
-import MyProfile from './components/MyProfile';
 import App from './App';
-import { fetchMissions, joinMission } from './redux/missions/MissionsSlice';
-import { fetchRockets, reserveRocket } from './redux/rockets/RocketsSlice';
-import Rockets from './components/Rockets';
 
 it('App renders correctly', () => {
   const tree = renderer
